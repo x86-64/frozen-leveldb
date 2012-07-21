97e38d13197f310a6f1f253c469cac032ada6ba0
x86_64 <x86mail@gmail.com>
2012-07-21 13:09:20 +0400

    Fix build_error script

--- src/errors_list.c
+++ src/errors_list.c
@@ -1,18 +1,17 @@
 
 static err_item errs_list[] = {
- { -334, "src/main_leveldb.c: leveldb_enum error" },
- { -322, "src/main_leveldb.c: leveldb_delete error" },
- { -310, "src/main_leveldb.c: leveldb_set error" },
- { -282, "src/main_leveldb.c: leveldb_get error" },
- { -266, "src/main_leveldb.c: no action specified" },
- { -186, "src/main_leveldb.c: leveldb_value_unserialize unknown output datatype: pass output key or set value_type in configuration" },
- { -180, "src/main_leveldb.c: leveldb_value_unserialize wrong output data supplied" },
- { -143, "src/main_leveldb.c: leveldb init failed" },
- { -131, "src/main_leveldb.c: leveldb configuration error: wrong management modes configuration" },
- { -125, "src/main_leveldb.c: invalid path specified" },
- { -77, "src/main_leveldb.c: calloc failed" },
+ { -534, "src/main_leveldb.c: leveldb_enum error" },
+ { -522, "src/main_leveldb.c: leveldb_delete error" },
+ { -510, "src/main_leveldb.c: leveldb_set error" },
+ { -482, "src/main_leveldb.c: leveldb_get error" },
+ { -466, "src/main_leveldb.c: no action specified" },
+ { -386, "src/main_leveldb.c: leveldb_value_unserialize unknown output datatype: pass output key or set value_type in configuration" },
+ { -380, "src/main_leveldb.c: leveldb_value_unserialize wrong output data supplied" },
+ { -343, "src/main_leveldb.c: leveldb init failed" },
+ { -331, "src/main_leveldb.c: leveldb configuration error: wrong management modes configuration" },
+ { -325, "src/main_leveldb.c: invalid path specified" },
+ { -277, "src/main_leveldb.c: calloc failed" },
 
-	{ 0, NULL }
 };
 #define            errs_list_size      sizeof(errs_list[0])
 #define            errs_list_nelements sizeof(errs_list) / errs_list_size
